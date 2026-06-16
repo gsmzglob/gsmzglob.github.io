01456d58c828060a0927add4c5138896a1818c0d
GSMZ GLOB <th3m9@proton.me>
2026-06-16 18:31:01 +0100

    Update 1.js

--- scripts/1.js
+++ scripts/1.js
@@ -1,16 +1,17 @@
 (function () {
-  const container = document.createElement("div");
-  container.style.width = "100%";
-  container.style.margin = "auto";
-  container.style.position = "relative";
-  container.style.zIndex = "99998";
+  const ins = document.createElement("ins");
+  ins.style.width = "320px";
+  ins.style.height = "50px";
+  ins.setAttribute("data-width", "320");
+  ins.setAttribute("data-height", "50");
+  ins.className = "nc9ac63b896";
+  ins.setAttribute("data-domain", "//data527.click");
+  ins.setAttribute("data-affquery", "/2d7a825db0aed0502e05/c9ac63b896/?placementName=banner");
 
-  container.innerHTML = `
-    <iframe data-aa="2444135"
-      src="//acceptable.a-ads.com/2444135/?size=Adaptive"
-      style="border:0; padding:0; width:70%; height:auto; overflow:hidden; display:block; margin:auto">
-    </iframe>
-  `;
+  const s = document.createElement("script");
+  s.src = "//data527.click/js/responsive.js";
+  s.async = true;
 
-  document.currentScript.parentNode.insertBefore(container, document.currentScript);
+  ins.appendChild(s);
+  document.currentScript.parentNode.insertBefore(ins, document.currentScript);
 })();
